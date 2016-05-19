6cbad1df249297a108513a8702cdbfbd5520eaa2
Lellis <laionel.lellis@outlook.com>
2016-05-19 01:00:08 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
 	/* Funcao responsavel por abrir o menu */
-	$('#menu .glyphicon-play').click(function() {
+	$('#menu .menuclick').click(function() {
 		if($('#menu').hasClass('menu-fechado')) {
 			$('#menu').animate({left: 0}, 600);
 			$('.glyphicon-play').css("-webkit-transform", "rotate(180deg)");
@@ -10,7 +10,6 @@ $(document).ready(function () {
 			$('.glyphicon-play').css("-ms-transform", "rotate(180deg)");
 			$('.glyphicon-play').css("-moz-transform", "rotate(180deg)");
 			$('.glyphicon-play').css("transform", "rotate(180deg)");
-			$('.glyphicon-play').css("padding", "20em 0.2em 1em 0.2em");
 		} else {
 			$('#menu').animate({left: '-15em'}, 600);
 			$('.glyphicon-play').css("-webkit-transform", "rotate(0deg)");
@@ -18,7 +17,6 @@ $(document).ready(function () {
 			$('.glyphicon-play').css("-ms-transform", "rotate(0deg)");
 			$('.glyphicon-play').css("-moz-transform", "rotate(0deg)");
 			$('.glyphicon-play').css("transform", "rotate(0deg)");
-			$('.glyphicon-play').css("padding", "1em 0.2em 20em 0.2em");
 		}
 		$('#menu').toggleClass('menu-fechado');
 	});
