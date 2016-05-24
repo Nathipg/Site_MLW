8ad0fc473fee334d19f2560e8b913415f1af6174
DarkLaion <laionel.lellis@outlook.com>
2016-05-24 16:28:30 -0300

    Funcionalidades
    Abra o menu e veja

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 			$('.glyphicon-play').css("-moz-transform", "rotate(180deg)");
 			$('.glyphicon-play').css("transform", "rotate(180deg)");
 		} else {
-			$('#menu').animate({left: '-17.77em'}, 600);
+			$('#menu').animate({left: '-18.2em'}, 600);
 			$('.glyphicon-play').css("-webkit-transform", "rotate(0deg)");
 			$('.glyphicon-play').css("-o-transform", "rotate(0deg)");
 			$('.glyphicon-play').css("-ms-transform", "rotate(0deg)");
@@ -19,8 +19,13 @@ $(document).ready(function () {
 			$('.glyphicon-play').css("transform", "rotate(0deg)");
 		}
 		$('#menu').toggleClass('menu-fechado');
+		/* Som de abertura do menu */
+		$('#sommenu').trigger('play');
 	});
-
+	/* Som do click no menu */
+	$('#menu ul li').click(function() {
+		$('#sommenuclick').trigger('play');
+	});
 	/* Funcao responsavel por abrir o menu de atalhos */
 	$('#menu-atalhos .menuclick').click(function() {
 		if($('#menu-atalhos').hasClass('menu-fechado')) {
