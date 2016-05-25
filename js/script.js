12662a1abe4d33cf1c600ecc33ace0799954f6d3
DarkLaion <laionel.lellis@outlook.com>
2016-05-25 11:03:20 -0300

    Controle Audio: Parcial
    Precisa corrigir click inicial

--- js/script.js
+++ js/script.js
@@ -10,7 +10,10 @@ $(document).ready(function () {
 			$('.glyphicon-play').css("-ms-transform", "rotate(180deg)");
 			$('.glyphicon-play').css("-moz-transform", "rotate(180deg)");
 			$('.glyphicon-play').css("transform", "rotate(180deg)");
-			$('#sommenuopen').trigger('play');/* Som ao abrir */
+			/* Som ao abrir */
+			if($('#somcontrol').hasClass('glyphicon-volume-up')) {
+				$('#sommenuopen').trigger('play');
+			}
 		} else {
 			$('#menu').animate({left: '-18.2em'}, 600);
 			$('.glyphicon-play').css("-webkit-transform", "rotate(0deg)");
@@ -18,22 +21,44 @@ $(document).ready(function () {
 			$('.glyphicon-play').css("-ms-transform", "rotate(0deg)");
 			$('.glyphicon-play').css("-moz-transform", "rotate(0deg)");
 			$('.glyphicon-play').css("transform", "rotate(0deg)");
-			$('#sommenuclose').trigger('play');/* Som ao fechar */
+			/* Som ao fechar */
+			if($('#somcontrol').hasClass('glyphicon-volume-up')) {
+				$('#sommenuclose').trigger('play');
+			}
 		}
 		$('#menu').toggleClass('menu-fechado');		
 	});
+	
 	/* Som do click no menu */
 	$('#menu ul li').click(function() {
-		$('#sommenuclick').trigger('play');
+		if($('#somcontrol').hasClass('glyphicon-volume-up')) {
+			$('#sommenuclick').trigger('play');
+		}
 	});
+	
+	/* Controle do som do site */
+	$('#somcontrol').click(function() {
+		if($('#somcontrol').hasClass('glyphicon-volume-off')) {
+			$('#somcontrol').toggleClass('glyphicon-volume-up');
+		} else {
+			$('#somcontrol').toggleClass('glyphicon-volume-off');
+		}
+	});
+	
 	/* Funcao responsavel por abrir o menu de atalhos */
 	$('#menu-atalhos .menuclick').click(function() {
 		if($('#menu-atalhos').hasClass('menu-fechado')) {
 			$('#menu-atalhos .texto-menu-aberto').show();
-			$('#sommenuopen').trigger('play');/* Som ao abrir */
+			/* Som ao abrir */
+			if($('#somcontrol').hasClass('glyphicon-volume-up')) {
+				$('#sommenuopen').trigger('play');
+			}
 		} else {
 			$('#menu-atalhos .texto-menu-aberto').hide();
-			$('#sommenuclose').trigger('play');/* Som ao fechar */
+			/* Som ao fechar */
+			if($('#somcontrol').hasClass('glyphicon-volume-up')) {
+				$('#sommenuclose').trigger('play');
+			}
 		}
 		$('#menu-atalhos').toggleClass('menu-fechado');
 	});
